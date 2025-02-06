2e67ee5baca05395c72a6cfb9f904b22a8c02c96
James House <james@volanterp.com>
2025-02-06 15:24:52 -0800

    removed modules and updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadComponent: () => import('./features/home/home/home.component').then(m => m.HomeComponent) },
+  { path: 'products', loadComponent: () => import('./features/products/product-list/product-list.component').then(m => m.ProductListComponent) },
   { path: '**', redirectTo: 'home' } // Wildcard route for unknown paths
 ];
